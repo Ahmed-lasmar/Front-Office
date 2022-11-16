cb78dfaf23ee05fa50c6e00a3751fcff02e6434c
Ahmed-lasmar <91993395+Ahmed-lasmar@users.noreply.github.com>
2022-11-16 18:15:09 +0100

    forgot mdp

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -2,9 +2,18 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
+use App\Form\ForgotPasswordType;
+use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Swift_Mailer;
+use Swift_Message;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+use Symfony\Component\Security\Csrf\TokenGenerator\TokenGeneratorInterface;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class SecurityController extends AbstractController
