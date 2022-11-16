06af98bdd49ecaa3afd76a710325f138bc48c820
Ahmed-lasmar <91993395+Ahmed-lasmar@users.noreply.github.com>
2022-11-16 15:42:07 +0100

    DB Emp not Final

---
+++ src/Controller/EmpController.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Conge;
+use App\Entity\Evenement;
+use App\Entity\Formation;
+use App\Form\CongeType;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+#[Route('/emp')]
+class EmpController extends AbstractController
+{
+    #[Route('/', name: 'app_emp')]
+    public function index(): Response
+    {
+        return $this->render('emp/index.html.twig', [
+            'controller_name' => 'EmpController',
+        ]);
+    }
+
+    #[Route('/Formation', name: 'app_formation_emp', methods: ['GET'])]
+    public function conFor(EntityManagerInterface $entityManager): Response
+    {
+        $formations = $entityManager
+            ->getRepository(Formation::class)
+            ->findAll();
+
+        return $this->render('emp/conFor.html.twig', [
+            'formations' => $formations,
+        ]);
+    }
+
+    #[Route('/Event', name: 'app_événements_emp', methods: ['GET'])]
+    public function conEvent(EntityManagerInterface $entityManager): Response
+    {
+        $evenements = $entityManager
+            ->getRepository(Evenement::class)
+            ->findAll();
+
+        return $this->render('emp/conEvent.html.twig', [
+            'evenements' => $evenements,
+        ]);
+    }
+
+    #[Route('/fichedepaie', name: 'app_fiche_de_paie_emp', methods: ['GET'])]
+    public function conFdP(EntityManagerInterface $entityManager): Response
+    {
+
+        return $this->render('emp/conFdP.html.twig');
+    }
+    #[Route('/conge', name: 'app_conge_emp', methods: ['GET', 'POST'])]
+    public function new(Request $request, EntityManagerInterface $entityManager): Response
+    {
+        $conge = new Conge();
+        $form = $this->createForm(DemConType::class, $conge);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->persist($conge);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_conge_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->renderForm('conge/new.html.twig', [
+            'conge' => $conge,
+            'form' => $form,
+        ]);
+    }
+}
