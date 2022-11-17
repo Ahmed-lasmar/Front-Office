9260f2ed192a1b2bd73a9d2bd950f762ed613630
ahmed mellouli <ahmed.mellouli@esprit.tn>
2022-11-17 08:38:03 +0100

    ajout controle saisie et modification formateur

---
+++ src/Form/FormateurType.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Formateur;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class FormateurType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('nom')
+            ->add('prenom')
+            ->add('adresse')
+            ->add('email')
+            ->add('tel')
+            ->add('codep')
+            ->add('ville')
+            ->add('pays')
+            ->add('status')
+            ->add('tarif')
+            ->add('tva')
+            ->add('bio')
+            ->add('diplome')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Formateur::class,
+        ]);
+    }
+}
